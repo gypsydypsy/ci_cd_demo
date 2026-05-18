c43c77e845c8fde93b980f871e6d4ecfd45ab4f3
DELL-LEA\leama <leamarie95@outlook.fr>
2026-05-18 12:48:43 +0200

    Test change on github pages

--- src/app/page.js
+++ src/app/page.js
@@ -28,7 +28,7 @@ export default function Home() {
           priority
         />
         <div className={styles.intro}>
-          <h1>Test change.</h1>
+          <h1>Test change update github pages.</h1>
           <p>
             Looking for a starting point or more instructions? Head over to{' '}
             <a
